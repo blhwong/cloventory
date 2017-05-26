ad421f2431a331093f7b5523a4bb75e15e03bfc0
Brandon Wong <b.lh.wong@gmail.com>
2017-05-26 12:11:53 -0700

    Clean up

--- your-code-goes-here/src/components/Inventory.jsx
+++ your-code-goes-here/src/components/Inventory.jsx
@@ -9,26 +9,19 @@ class Inventory extends Component {
   }
 
   handleClick(e) {
-    console.log(e.target);
     if (e.target.innerHTML[0] === 'I') {
       if (this.props.sortNameAscending) {
-        console.log('sort name descending');
         this.props.getItems(this.props.setInventory, '-name');
         this.props.setSortState(false, 'name');
       } else {
-        console.log('sort name ascending');
         this.props.getItems(this.props.setInventory, 'name');
         this.props.setSortState(true, 'name');
       }
     } else {
       this.props.sortQuantity();
       if (this.props.sortQuantityAscending) {
-        console.log('sort quantity descending');
-        // this.props.getItems(this.props.setInventory, '-quantity');
         this.props.setSortState(false, 'quantity');
       } else {
-        console.log('sort quantity ascending');
-        // this.props.getItems(this.props.setInventory, 'quantity');
         this.props.setSortState(true, 'quantity');
       }
     }
