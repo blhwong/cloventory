fc4609c5ed735c45ce61f39bfd8153edfc83ab25
Brandon Wong <b.lh.wong@gmail.com>
2017-05-25 22:06:26 -0700

    Create item and quantity is working

--- your-code-goes-here/src/lib/clover.jsx
+++ your-code-goes-here/src/lib/clover.jsx
@@ -1,18 +1,36 @@
 import axios from 'axios';
 
-const getItems = () => {
+const getItems = (cb) => {
   console.log('getting items...');
   axios.get(`https://apisandbox.dev.clover.com/v3/merchants/${localStorage.merchantId}/items?access_token=${localStorage.accessToken}`)
   .then((results) => {
-    console.log(results);
+    cb(results);
   })
   .catch((err) => {
     console.error(err);
   });
 };
 
-const createItem = () => {
-  console.log('creating item...');
+const createItem = (name = 'hi', quantity = 15, cb) => {
+  // console.log('creating item...');
+  let data = {
+    name: 'passionfruit',
+    price: 110
+  };
+  axios.post(`https://apisandbox.dev.clover.com/v3/merchants/${localStorage.merchantId}/items?access_token=${localStorage.accessToken}`, data)
+  .then((result) => {
+    console.log('Success 1', result);
+    let quantityData = {
+      quantity: 66
+    };
+    return axios.post(`https://apisandbox.dev.clover.com/v3/merchants/${localStorage.merchantId}/item_stocks/${result.data.id}?access_token=${localStorage.accessToken}`, quantityData);
+  })
+  .then((result2) => {
+    console.log('Success 2', result2);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 };
 
 const deleteItem = () => {
@@ -21,6 +39,13 @@ const deleteItem = () => {
 
 const updateItem = () => {
   console.log('updating item...');
+  // axios.post(`https://apisandbox.dev.clover.com/v3/merchants/${localStorage.merchantId}/items?access_token=${localStorage.accessToken}`, data)
+  // .then((result) => {
+  //   console.log(result);
+  // })
+  // .catch((err) => {
+  //   console.error(err);
+  // });
 };
 
 export default {
