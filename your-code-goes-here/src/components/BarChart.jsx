a578369fdf23c416ce2f3058a66ccc8e322c8dc1
Brandon Wong <b.lh.wong@gmail.com>
2017-05-26 12:07:12 -0700

    Clean up

--- your-code-goes-here/src/components/BarChart.jsx
+++ your-code-goes-here/src/components/BarChart.jsx
@@ -4,59 +4,24 @@ import { Bar } from 'react-chartjs-2';
 export default class BarChart extends Component {
   constructor(props) {
     super(props);
-    // this.state = {
-    //   avgEmoji: ''
-    // };
-    // this.mostFreqEmoji = this.mostFreqEmoji.bind(this);
   }
-
-  // mostFreqEmoji(emojis) {
-  //   emojis = emojis.replace('undefined', '');
-  //   let emojiStorage = {};
-  //   let freqEmojiCounter = 0;
-
-  //   for (let i = 0; i < emojis.length; i += 2) {
-  //     let emoji = emojis.substr(i, 2);
-  //     isNaN(emojiStorage[emoji]) ? emojiStorage[emoji] = 0 : emojiStorage[emoji]++;
-  //     if ( emojiStorage[emoji] > freqEmojiCounter) {
-  //       freqEmojiCounter = emojiStorage[emoji];
-  //       this.state.avgEmoji = emoji;
-  //     }
-  //   }
-  // }
 
   render() {
     let labels = [];
     let data = [];
     this.props.items.forEach((item) => {
-      // data.push([item.name, item.stockCount]);
       labels.push(item.name);
       data.push(item.stockCount);
     });
     console.log('barchartdata', data);
-    // const {avg_data} = this.props;
-    // const praseAvgData = JSON.parse(avg_data);
-    // let avgChartData = [];
-
-    // for (var emotion in praseAvgData) {
-    //   if ( emotion !== 'emoji') {
-    //     avgChartData.push( Math.floor(praseAvgData[emotion] * 100));
-    //   } else {
-    //     let emojis = praseAvgData[emotion];
-    //     this.mostFreqEmoji(emojis);
-    //   }
-    // }
-
     const barAvgData = {
       labels: labels,
       datasets: [
         {
           label: 'Inventory',
           backgroundColor: 'rgba(67, 176, 42, 1)',
-          // borderColor: 'rgba(235, 84, 36, 0.80)',
           borderWidth: 1,
           hoverBackgroundColor: 'rgba(0, 121, 63, 1)',
-          // hoverBorderColor: 'rgba(235, 84, 36, 1)',
           data: data
         }
       ]
@@ -80,9 +45,7 @@ export default class BarChart extends Component {
             display: false
           },
           ticks: {
-            // max: 100,
             min: 0,
-            // stepSize: 10
           }
         }]
       }
@@ -100,4 +63,3 @@ export default class BarChart extends Component {
     );
   }
 }
-
